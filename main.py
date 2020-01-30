11511f5594ce56c5f44a28a1eaaf37d49087b48b
shunail2029 <tiger1998nov27@gmail.com>
2020-01-30 17:44:31 +0900

    fixed main.py for pub/sub

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 from mypackage import *
 
-def update_calendar():
+def update_calendar(event, context):
     # build service of google calendar
     service = mygoogle.build_service()
 
